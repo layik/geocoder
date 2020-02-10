5b9617d4aeb62addbfcd729c926031109ab4e80d
Layik Hama <layik.hama@gmail.com>
2020-02-10 10:46:42 +0000

    more tests on setup

--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -11,3 +11,14 @@ test_that("Setenv works", {
   host = Sys.getenv("MONGODB_HOST")
   expect_equal(setup(), "mongodb://localhost:27017")
 })
+
+test_that("Jeroen's demo server", {
+  j = "mongodb://readwrite:test@mongo.opencpu.org:43942/jeroen_test"
+  # con <- mongo("mtcars", url = j)
+  Sys.setenv(MONGODB_HOST="mongo.opencpu.org")
+  Sys.setenv(MONGODB_USER="readwrite")
+  Sys.setenv(MONGODB_PASS="test")
+  Sys.setenv(MONGODB_PORT="43942")
+  expect_equal(setup("jeroen_test"), j)
+
+})
