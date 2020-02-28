7429d5c420f2b6006ca011de707d06d093e3b885
Layik Hama <layik.hama@gmail.com>
2020-02-28 00:10:33 +0000

    gc-query tdd

--- R/utils.R
+++ R/utils.R
@@ -1,30 +1,32 @@
 #' Generate JSON MongoDB spatial query
 #'
-#' See
-#' https://docs.mongodb.com/manual/reference/operator/query-geospatial/
+#' See MongoDB \url{
+#' https://docs.mongodb.com/manual/reference/operator/query-geospatial/}
+#' for details of spatial queries. This functions generates only:
+#' `geoIntersects`, `geoWithin`, `near` or `nearSphere`.
+#'
 #'
 #' @return JSON valid MongoDB query
 #'
-#' @param ... different params. Requires query type which must be
+#' @param ... different params.
+#' @param query MongoDB query type which must be
 #' one of `geoIntersects`, `geoWithin`, `near` or `nearSphere`.
+#' @param type geom type which must be a valid GeoJSON type
+#' @param coords list of coordinates which matches `type`
+#' Or just provide a valid sf with one row as both type and coords.
 #'
 #' @export
-#' @example
-#' \dontrun{
-#' gc_query(query="near", type = "point", coordinates = sf::st_point(0,1))
+#' @example {
+#' gc_query(query="near", type = "point", coords = c(0,1))
 #' }
-gc_query = function(...) {
+gc_query = function(query, type, coords, ...) {
   qtypes = c("geoIntersects", "geoWithin", "near", "nearSphere")
   geom.types = c("Point", "MultiPoint", "LineString", "MultiLineString",
                  "Polygon", "MultiPolygon", "GeometryCollection")
-  input = list(...)
-  query = input[['query']]
   if(is.null(query) || nchar(query) == 0) {
     stop("MongoDB spatial query type is required: ",
          paste(qtypes, collapse = ", "), ".")
   }
-  type = input[["type"]]
-  coords = input[["coordinates"]]
   # build geoIntersects
   if(query == "geoIntersects") {
     if(is.null(type)) {
@@ -40,8 +42,65 @@ gc_query = function(...) {
   }
 
   # build geoWithin
+  if(query == "geoWithin") {
+    geoWithinTypes = c("Polygon", "MultiPolygonPolygon")
+    if(is.null(type) || !any(grepl(type, geoWithinTypes))){
+      stop("Type '", type, "' must be one of: ",
+           paste(geoWithinTypes, collapse = ", "), ".")
+    }
+    if(is.null(coords)) {
+      stop("For query '", query, "' coordinates are required.")
+    }
+  }
+  # build near or nearSphere
+  input = list(...)
+  maxDistance = input[["maxDistance"]]
+  minDistance = input[["minDistance"]]
+  if(query == "near" || query == "nearSphere") {
+    if(!is.null(type) && tolower(type) != "point") {
+      warning("For near query, type must be a Point. Ignoring: ", type)
+    }
+    if(length(coords) != 2) {
+      stop("For near query, only two coordinates is needed.")
+    }
+    if(!is.null(maxDistance) || !is.null(minDistance)) {
+      if(!is.numeric(maxDistance) || !is.numeric(minDistance)) {
+        stop("For near query, max and min distance must be numeric.")
+      }
+    }
+  }
 
-  # build near
-
-  # build nearSphere
+  # generate the JSON
+  r = paste0('{ "$', query, '":{ geometry": ')
+  # generate the geometry
+  make_json = function(sf_func) {
+    g = sf::st_sfc(sf_func(coords))
+    geojsonsf::sfc_geojson(g)
+  }
+  geom = NULL
+  if(tolower(type) == "point") {
+    geom = make_json(sf::st_point)
+  } else {
+    switch(tolower(type),
+           multipoint={
+             geom = make_json(sf::st_multipoint)
+           },
+           linestring={
+             geom = make_json(sf::st_linestring)
+           },
+           multilinestring={
+             geom = make_json(sf::st_multilinestring)
+           },
+           polygon={
+             geom = make_json(sf::st_polygon)
+           },
+           multipolygon={
+             geom = make_json(sf::st_multipolygon)
+           },
+           stop("Unknown geojson type.")
+    )
+  }
+  r = paste0(r, geom,' } } }')
+  jsonify::validate_json(r)
+  r
 }
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,8 +1,23 @@
+context("gc-query")
 test_that("nothing returned", {
   expect_error(gc_query())
-  expect_silent(gc_query(query = "near", type = "point"))
   expect_error(gc_query(query = "geoIntersects", type = "point"))
   expect_error(gc_query(query = "geoIntersects"))
+})
+
+test_that("geoWithin works", {
+  expect_error(gc_query(query = "geoWithin", type = "point"))
+  expect_error(gc_query(query = "geoWithin"))
+})
+
+test_that("near works", {
+  expect_error(gc_query(query = "near", type = "point",
+                        coords = c(0,1),
+                        maxDistance = "1",
+                        minDistance = "2"))
+  expect_error(gc_query(query = "near", type = "point", coords = c(0,1,2)))
+  expect_error(gc_query(query = "near", type = "point"))
+  expect_error(gc_query(query = "near"))
 })
 
 test_that("GeoJSON geomtypes work", {
@@ -13,4 +28,41 @@ test_that("GeoJSON geomtypes work", {
   expect_error(gc_query(query = "geoIntersects", type = "linestring"))
   expect_error(gc_query(query = "geoIntersects", type = "multilinestringg"))
   expect_error(gc_query(query = "geoIntersects", type = "GeometryCollectionn"))
+})
+
+test_that("generate query", {
+  expect_error(gc_query(query = "geoIntersects", type = "linestring"),
+               "argument \"coords\"")
+  expect_is(gc_query(query = "geoIntersects", type = "linestring",
+                          coords = matrix(1:6, ncol = 2)),
+                  "character")
+  expect_error(gc_query(query = "geoIntersects", type = "polygon",
+                     coords = matrix(1:6, ncol = 2)))
+  pts = matrix(1:15, , 3)
+  expect_is(gc_query(query = "geoIntersects", type = "multipoint",
+                     coords = pts),
+            "character")
+  # (mp2 = st_multipoint(pts))
+  # thanks https://r-spatial.github.io/sf/reference/st.html
+  outer = matrix(c(0,0,10,0,10,10,0,10,0,0),ncol=2, byrow=TRUE)
+  hole1 = matrix(c(1,1,1,2,2,2,2,1,1,1),ncol=2, byrow=TRUE)
+  hole2 = matrix(c(5,5,5,6,6,6,6,5,5,5),ncol=2, byrow=TRUE)
+  pts = list(outer, hole1, hole2)
+  pts3 = lapply(pts, function(x) cbind(x, 0))
+  # (ml2 = st_multilinestring(pts3))
+  expect_is(gc_query(query = "geoIntersects", type = "multilinestring",
+                     coords = pts3),
+            "character")
+  # (pl1 = st_polygon(pts))
+  pol1 = list(outer, hole1, hole2)
+  pol2 = list(outer + 12, hole1 + 12)
+  pol3 = list(outer + 24)
+  mp = list(pol1,pol2,pol3)
+  # (mp1 = st_multipolygon(mp))
+  expect_is(gc_query(query = "geoIntersects", type = "multipolygon",
+                     coords = mp),
+            "character")
+  expect_is(gc_query(query = "geoIntersects", type = "polygon",
+                     coords = pts),
+            "character")
 })
